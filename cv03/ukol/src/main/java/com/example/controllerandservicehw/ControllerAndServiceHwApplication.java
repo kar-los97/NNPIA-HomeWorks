252c16692d07e5cb5b9f6441a191ea85cf4a73b7
Karel Andres <kajusuandr97@gmail.com>
2022-03-07 17:36:47 +0100

    NNPIA - úkol cv. 4 ORM a JPA

---
+++ cv03/ukol/src/main/java/com/example/controllerandservicehw/ControllerAndServiceHwApplication.java
@@ -0,0 +1,13 @@
+package com.example.controllerandservicehw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllerAndServiceHwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControllerAndServiceHwApplication.class, args);
+    }
+
+}
---
+++ cv03/ukol/src/test/java/com/example/controllerandservicehw/ControllerAndServiceHwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.controllerandservicehw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllerAndServiceHwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
